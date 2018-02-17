6e427aa1a5e520155a77a49804feb7d0dfade468
piaga <andrea_vcc@hotmail.it>
2018-02-17 05:16:41 +0100

    Restyling 0.4

--- dashboard/src/app/hud/sensors/hud-battery/hud-battery.component.ts
+++ dashboard/src/app/hud/sensors/hud-battery/hud-battery.component.ts
@@ -9,6 +9,7 @@ import { HudDataService } from '../../hud-data.service';
 export class HudBatteryComponent implements OnInit {
 
   private value: number;
+  private loading: boolean;
   private sensor_type="BATTERY";
   private sensorProperties:{[key:string]:any};
 
@@ -16,7 +17,11 @@ export class HudBatteryComponent implements OnInit {
 
   getSensor(): void {
     this.dataService.getSensorObservable(this.sensor_type).subscribe(sensor=>{
-      this.value = sensor.data.value;
+      
+      let value = sensor.data.value;
+      if (value>this.value) this.loading=true;
+      else this.loading=false;
+      this.value=value;
       if (this.sensorProperties==null&&sensor.properties!=null&&sensor.properties!=undefined)
       {
         this.sensorProperties=sensor.properties;
--- dashboard/src/app/hud/sensors/hud-camera/hud-camera.component.ts
+++ dashboard/src/app/hud/sensors/hud-camera/hud-camera.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { HudDataService } from '../../hud-data.service';
+import { HUD_SENSORS_DETAIL_NAME } from '../../model/hud-sensors-detail-enum';
 
 @Component({
   selector: 'app-hud-camera',
@@ -12,7 +13,8 @@ export class HudCameraComponent implements OnInit {
 
   frame: string = "bm90IGFjdHVhbGx5IGEganBlZyBmaWxlCg==";
   id: string= "cameraScreen";
-
+  image_width:number;
+  image_height:number;
   
   blob: Blob;
   src: SafeUrl;
@@ -26,6 +28,13 @@ export class HudCameraComponent implements OnInit {
       fetch(url)
       .then(res => res.blob())
       .then(blob => this.src = this._sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(blob)));
+
+      if (sensor.properties[HUD_SENSORS_DETAIL_NAME.RESOLUTION]!=undefined && this.image_width==0 && this.image_height==0)
+      {
+        let resolution:string=sensor.properties[HUD_SENSORS_DETAIL_NAME.RESOLUTION];
+        this.image_width=parseInt(resolution.substring(1,resolution.length-2).split('x')[0]);
+        this.image_height=parseInt(resolution.substring(1,resolution.length-2).split('x')[1]);
+      }
     })
   }
 
